3b37bd2425ad481b4f52429f6420f9421e4afd57
Rabia-Ahm <rabiamtd@gmail.com>
2024-05-30 17:00:36 +0200

    fix localstorage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 "use strict";
-// Check localStorage for saved data on narrative shown and answered questions
+
+// Check localStorage for saved data on narrative shown, answered questions
 const savedSites = JSON.parse(localStorage.getItem('sites'));
 const answeredQuestions = JSON.parse(localStorage.getItem('answeredQuestions')) || [];
-
+// Check localStorage for a saved username
+const savedUsername = localStorage.getItem('username');
 
 // If saved data exists, create the map page
-if (answeredQuestions.length != 0) {
+if (answeredQuestions.length != 0 || savedUsername) {
     createMapPage();
 } else {
     // Save the initial site data to localStorage
--- pages/loading_screen.js
+++ pages/loading_screen.js
@@ -28,46 +28,46 @@ function createLoadingScreenPage() {
 function createStartPage() {
     main.classList.remove("loadingScreenMain");
     main.innerHTML = `
-<div class="StartPageContainer">
-    <div class="rectangle-logo-container"> 
-        <img class="rectangle" alt="logo image of a boat tour company" src="images/logo.jpg">
-        <button class="mainBtn" id="startTipsrundaBtn" type="submit">Start boat tour</button>
-    </div>
-</div> 
-`;
+    <div class="StartPageContainer">
+        <div class="rectangle-logo-container"> 
+            <img class="rectangle" alt="logo image of a boat tour company" src="images/logo.jpg">
+            <button class="mainBtn" id="startTipsrundaBtn" type="submit">Start boat tour</button>
+        </div>
+    </div> 
+    `;
 
     document.getElementById("startTipsrundaBtn").addEventListener("click", createAllowLocationPermissionPage);
 }
 
 function createAllowLocationPermissionPage() {
     main.innerHTML = `
-<div class="locationPermissionPageContainer">
-    <div id="allowPermissionBoxContainer">
-        <p>Malmö Kanalrundan needs access to your location in the background in order to inform you about interesting things near you and unlock the questions you come across. Go to your device's settings to turn on location permissions and select "Allow All The Time".</p>
-        <button class="mainBtn" id="location-ok-btn">OK</button>
+    <div class="locationPermissionPageContainer">
+        <div id="allowPermissionBoxContainer">
+            <p>Malmö Kanalrundan needs access to your location in the background in order to inform you about interesting things near you and unlock the questions you come across. Go to your device's settings to turn on location permissions and select "Allow All The Time".</p>
+            <button class="mainBtn" id="location-ok-btn">OK</button>
+        </div>
     </div>
-</div>
-`;
+    `;
 
     document.getElementById("location-ok-btn").addEventListener("click", createIntroPage);
 }
 
 function createIntroPage() {
     main.innerHTML = `
-<div class="introPageContainer">
-    <div class="introBoxContainer">
-        <h1>MCSP</h1>
-        <div class="introDialogueContainer">
-            <p>Hej there! Welcome to the Memory Calibration and Synchronization Protocol. I'm Astrid, your virtual guide designed by MCSP.</p>
-            <p>Oh, but before we dive in, what should I call you?</p>
-        </div>
-        <div>
-            <input type="text" id="username-input" placeholder="Your name">
-            <button class="mainBtn" id="username-submit-btn">Submit</button>
+    <div class="introPageContainer">
+        <div class="introBoxContainer">
+            <h1>MCSP</h1>
+            <div class="introDialogueContainer">
+                <p>Hej there! Welcome to the Memory Calibration and Synchronization Protocol. I'm Astrid, your virtual guide designed by MCSP.</p>
+                <p>Oh, but before we dive in, what should I call you?</p>
+            </div>
+            <div>
+                <input type="text" id="username-input" placeholder="Your name">
+                <button class="mainBtn" id="username-submit-btn">Submit</button>
+            </div>
         </div>
     </div>
-</div>
-`;
+    `;
 
     document.getElementById("username-submit-btn").addEventListener("click", handleUsernameSubmit);
 }
@@ -84,20 +84,19 @@ function handleUsernameSubmit() {
 }
 
 function createIntroPageWithUsername(username) {
-    main.innerHTML =
-        `<div class="introPageContainer">
-            <div class="introBoxContainer">
-                <h1>MCSP</h1>
-                <div class="introDialogueContainer">
-                    <p>Hej <span style="color:#8ecae6">${username}</span>! Malmö Kanalrundan is MCSP's way of blending history with modern tech to keep Malmö's story alive.</p>
-                    <p>And with your help, we're making sure this city's rich past isn't forgotten.</p>
-                    <p>So, <span style="color:#8ecae6">${username}</span>, ready to join me on this journey through time and memory?</p>
-                </div>
-
-                <button class="mainBtn" id="lets-go-button">Let's go!</button>
-                </div>
+    main.innerHTML = `
+    <div class="introPageContainer">
+        <div class="introBoxContainer">
+            <h1>MCSP</h1>
+            <div class="introDialogueContainer">
+                <p>Hej <span style="color:#8ecae6">${username}</span>! Malmö Kanalrundan is MCSP's way of blending history with modern tech to keep Malmö's story alive.</p>
+                <p>And with your help, we're making sure this city's rich past isn't forgotten.</p>
+                <p>So, <span style="color:#8ecae6">${username}</span>, ready to join me on this journey through time and memory?</p>
             </div>
-        </div>`;
+            <button class="mainBtn" id="lets-go-button">Let's go!</button>
+        </div>
+    </div>
+    `;
 
     document.getElementById("lets-go-button").addEventListener("click", createMapPage);
 }
